9fa812040a7aa7efaa1fc355785a937fb5191cca
kaminigohatre <kaminigohatre05255@gmail.com>
2025-06-12 19:04:28 +0530

    Update AppTest.java

--- src/test/java/com/project/AppTest.java
+++ src/test/java/com/project/AppTest.java
@@ -8,19 +8,19 @@ public class AppTest {
     @Test
     public void testLogin1() {
         App myApp = new App();
-        Assert.assertEquals(0, myApp.userLogin("abc", "abc123"));
+        Assert.assertEquals(0, myApp.userLogin("abc", "abc@123"));
     }
 
     @Test
     public void testLogin2() {
         App myApp = new App();
-        Assert.assertEquals(1, myApp.userLogin("abc", "abc@123"));
+        Assert.assertEquals(1, myApp.userLogin("abc", "abc123"));
     }
 
     @Test
     public void testLogin3() {  // Renamed to testLogin3
         App myApp = new App();
-        Assert.assertEquals(1, myApp.userLogin("abc", "abc@1234"));
+        Assert.assertEquals(1, myApp.userLogin("abc", "abc1234"));
     }
 }
 
